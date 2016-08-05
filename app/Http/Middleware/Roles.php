c2a40f471a35ca8b3ce762a25109969d9283b454
Redlein <redlein7@gmail.com>
2016-08-05 09:01:23 -0500

    roles parte 2

--- app/Http/Middleware/Roles.php
+++ app/Http/Middleware/Roles.php
@@ -17,8 +17,28 @@ class Roles
     public function handle($request, Closure $next, $grupo)
     {
         if ($grupo == 'grupo1') {
-            if (Auth::user()->usuariotipo_id == ) {
-                # code...
+            if (Auth::user()->usuariotipo_id != 3 || Auth::user()->usuariotipo_id != 6) {
+                dd("No estas autorizado");
+            }
+        }
+        if ($grupo == 'grupo2') {
+            if (Auth::user()->usuariotipo_id != 3 || Auth::user()->usuariotipo_id != 4 || Auth::user()->usuariotipo_id != 6) {
+                dd("No estas autorizado");
+            }!=
+        }
+        if ($grupo == 'grupo3') {
+            if (Auth::user()->usuariotipo_id != 3 || Auth::user()->usuariotipo_id != 4) {
+                dd("No estas autorizado");
+            }
+        }
+        if ($grupo == 'grupo4') {
+            if (Auth::user()->usuariotipo_id != 6) {
+                dd("No estas autorizado");
+            }
+        }
+        if ($grupo == 'grupo5') {
+            if (Auth::user()->usuariotipo_id != 4 || Auth::user()->usuariotipo_id != 6) {
+                dd("No estas autorizado");
             }
         }
         return $next($request);
