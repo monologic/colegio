ff2ace9a468ad9629ba40c42078b80ebcf9ddf10
Redlein <redlein7@gmail.com>
2016-08-11 19:11:43 -0500

    visualizar archivo

--- resources/views/biblioteca/visualizar.blade.php
+++ resources/views/biblioteca/visualizar.blade.php
@@ -7,7 +7,7 @@
 			<div class="cinta">
 				
 			</div>
-		<iframe id="frame" src="https://drive.google.com/viewerng/viewer?url=http://robert.runait.com/archivos/archivo_1470752586.pdf?pid=explorer&efh=false&a=v&chrome=false&embedded=true" width="90%" height="700px">
+		<iframe id="frame" src="https://drive.google.com/viewerng/viewer?url=http://robert.runait.com/archivos/{{ $archivo->archivo }}?pid=explorer&efh=false&a=v&chrome=false&embedded=true" width="90%" height="700px"></iframe>
 			
 		</iframe>
 	</div>
