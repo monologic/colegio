13f1f43383f06922d8ee3827b4cdb7ff4b0dd95a
xxmaucoxx <btr_kirito1@hotmail.com>
2016-08-16 16:32:11 -0500

    full sream end(A festejarr!!)
    gooo

--- resources/views/biblioteca/visualizar.blade.php
+++ resources/views/biblioteca/visualizar.blade.php
@@ -7,9 +7,32 @@
 		@if ($archivo->archivotipo_id == 3)
 		    <iframe width="100%" src="{{ $archivo->archivo }}" frameborder="0" style="height:400px"></iframe>
 		@else
-			<div class="cinta">	
+			<div class="cont-archivo">
+				<div class="cinta"> 
+					<div class="cont-btns">
+						<button  class="btn btn-full" id="btn-n" onclick="normal()" disabled="true"><i class="glyphicon glyphicon-resize-small"></i></button>
+					<button  class="btn btn-full" id="btn-f" onclick="full()"><i class="glyphicon glyphicon-fullscreen"></i></button>
+					</div>
+						
+				</div>
+				<iframe id="frame" src="https://drive.google.com/viewerng/viewer?url=http://robert.runait.com/archivos/{{ $archivo->archivo }}?pid=explorer&efh=false&a=v&chrome=false&embedded=true"  width="100%"></iframe>
 			</div>
-			<iframe id="frame" src="https://drive.google.com/viewerng/viewer?url=http://robert.runait.com/archivos/{{ $archivo->archivo }}?pid=explorer&efh=false&a=v&chrome=false&embedded=true" width="90%" style="height: 600px"></iframe>
+			
 		@endif
 	</div>
+	 <script>
+        function full()
+        {
+    		$('.cont-archivo').addClass('fullScream');
+    		$('#frame').css('height','100%');
+    		$( "#btn-f" ).prop( "disabled", true );
+    		$( "#btn-n" ).prop( "disabled", false );
+        }
+        function normal(){
+        	$('.cont-archivo').removeClass('fullScream');
+    		$('#frame').css('height','500px');
+    		$( "#btn-f" ).prop( "disabled", false );
+    		$( "#btn-n" ).prop( "disabled", true );
+        }
+    </script>
 @endsection
--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -154,6 +154,7 @@
 					$('.main-panel').addClass('movilpanel');
 				} 
 		}
+		
 	</script>
 		
 	</body>
